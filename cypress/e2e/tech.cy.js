19c3d52a6a6a924ea8857ea56b4bb84c23031d0a
khomus <bekar@tut.by>
2024-01-09 10:17:02 +0100

    Readme edit

--- cypress/e2e/tech.cy.js
+++ cypress/e2e/tech.cy.js
@@ -4,7 +4,7 @@ describe("Tech Interview", () => {
     
   it("Test 1", () =>{
      
-      cy.visit('https://news.zerkalo.io/economics/58207.html')  
+      cy.visit('http://news.zerkalo.io/economics/58207.html')  
       cy.log('---Starting searching for Search field')
       cy.get("search_from_str").should('be.visible');
       
