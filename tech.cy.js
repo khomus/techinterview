710493c0269973421677b0ec711abf450a5e966b
khomus <bekar@tut.by>
2024-01-09 08:32:13 +0100

    First update

--- tech.cy.js
+++ tech.cy.js
@@ -1,14 +1,11 @@
 //import {} from ""
 
 describe("Tech Interview", () => {
-
-    before( function() {
-        //cy.visit('/');
-    })
     
     it("Test 1", () =>{
        
         cy.visit('www.zerkalo.io')  
+        
         
     })
 })
