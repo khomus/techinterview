3558b8ad2562181414a1121d27282159477cb9d3
khomus <bekar@tut.by>
2024-01-09 08:30:39 +0100

    Initial commit Tech Interviews

---
+++ cypress/e2e/tech.cy.js
@@ -0,0 +1,10 @@
+//import {} from ""
+
+describe("Tech Interview", () => {
+    
+  it("Test 1", () =>{
+     
+      cy.visit('www.zerkalo.io')  
+      
+  })
+})
---
+++ tech.cy.js
@@ -0,0 +1,14 @@
+//import {} from ""
+
+describe("Tech Interview", () => {
+
+    before( function() {
+        //cy.visit('/');
+    })
+    
+    it("Test 1", () =>{
+       
+        cy.visit('www.zerkalo.io')  
+        
+    })
+})
