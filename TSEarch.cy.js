ff1cbafff765b12f80d30fde9ba30aa56a83276f
khomus <bekar@tut.by>
2024-01-11 15:11:24 +0100

    Commit tech interview

---
+++ TSEarch.cy.js
@@ -0,0 +1,118 @@
+import {generateUserData } from "../../support/helpers/userHelper.js"
+
+describe("Front Page", () => {
+
+    let user = generateUserData()
+
+    before( function() {
+        cy.createAuthenticatedUser(user)
+        cy.login(user.email,user.password)
+        cy.visit('/');
+
+    })
+    
+    it("[C40177] User/Guest/Admin is able to click on Donor from Donations section", () =>{
+
+         //Check if User has access 
+
+        cy.get('.ts-catalog-promo__logo').first().invoke('attr','href')
+        .then(($getUrl) =>{ 
+            cy.get('.ts-catalog-promo__logo').first().click()
+            cy.get('#ts-stock-catalog').should('exist')
+            cy.url().should('include', $getUrl )
+            cy.visit('/user/logout')  
+        })
+
+        //Check if Admin has access 
+
+        cy.loginAsAdmin()
+        cy.reload()
+        cy.get('.ts-catalog-promo__logo').first().invoke('attr','href')
+        .then(($getUrl) =>{ 
+            cy.get('.ts-catalog-promo__logo').first().click()
+            cy.get('#ts-stock-catalog').should('exist')
+            cy.url().should('include', $getUrl )
+            cy.visit('/user/logout')  
+        })
+
+        //Check if Gueast has access 
+
+        cy.get('.ts-catalog-promo__logo').first().invoke('attr','href')
+        .then(($getUrl) =>{ 
+            cy.get('.ts-catalog-promo__logo').first().click()
+            cy.get('#ts-stock-catalog').should('exist')
+            cy.url().should('include', $getUrl )
+            cy.visit('/user/logout')  
+        })  
+    })
+
+it("[C40178] User/Guest/Admin is able to click on Product from Most Popular Donations section", () => {
+    //Check if User has access    
+  
+    cy.get('.ts-product-card').last().invoke('attr','href')
+    .then(($getUrl) => { 
+        cy.get('.ts-product-card').last().click()
+        cy.get('.node-content-tab-menu__list').should('exist')
+        cy.url().should('include', $getUrl)
+        cy.visit('/user/logout')  
+            
+    })
+
+   //Check if Admin has access  
+
+    cy.loginAsAdmin()
+    cy.reload()
+    cy.get('.ts-product-card').last().invoke('attr','href')
+    .then(($getUrl) => { 
+        cy.get('.ts-product-card').last().click()
+        cy.get('.node-content-tab-menu__list').should('exist')
+        cy.url().should('include', $getUrl)
+        cy.visit('/user/logout')  
+        
+    })
+
+   //Check if Guest has access
+
+    cy.get('.ts-product-card').last().invoke('attr','href')
+    .then(($getUrl) => { 
+        cy.get('.ts-product-card').last().click()
+        cy.get('.node-content-tab-menu__list').should('exist')
+        cy.url().should('include', $getUrl)
+            
+    })
+
+ })
+
+ it("[C40181] User/Guest/Admin is able to use social media icons", () => {
+    //Check if User has access    
+    cy.log('--- Starting [C40181] User is able to use social media icons ---') 
+    cy.get('[class=list-no-style').children().eq(2).should('be.visible');
+    cy.get('[class=list-no-style').children().eq(2).children().should('have.attr', 'href').then((href) => {
+       cy.visit(href)
+    });
+    cy.go('back');
+    cy.visit('/user/logout')  
+            
+    //Check if Admin has access  
+    cy.loginAsAdmin()
+    cy.reload()
+    cy.log('--- Starting [C40181] Admin is able to use social media icons ---') 
+    cy.get('[class=list-no-style').children().eq(2).should('be.visible');
+    cy.get('[class=list-no-style').children().eq(2).children().should('have.attr', 'href').then((href) => {
+       cy.visit(href)
+    });
+    cy.go('back');
+    cy.visit('/user/logout') 
+             
+    //Check if Guest has access
+    cy.log('--- Starting [C40181] Guest is able to use social media icons ---') 
+    cy.get('[class=list-no-style').children().eq(2).should('be.visible');
+    cy.get('[class=list-no-style').children().eq(2).children().should('have.attr', 'href').then((href) => {
+       cy.visit(href)
+    });
+    cy.go('back');
+})
+
+
+
+})
--- cypress/e2e/tech2.cy.js
+++ cypress/e2e/tech2.cy.js
@@ -6,10 +6,14 @@ describe("Tech Interview", () => {
   it("Test 1", () =>{
      
       cy.visit('http://www.skyscanner.net?previousCultureSource=COOKIE&redirectedFrom=www.skyscanner.net&locale=en-GB')  
-      cy.get('#destinationInput-input').click().then() => {
-         cy.get('.BpkButton_bpk-button__MDk1Y BpkButton_bpk-button--link__NGVhY FilterButton_button__Mzc2N').click()
-      }
-         
+      cy.get('#destinationInput-input').click().get('.BpkButton_bpk-button__MDk1Y BpkButton_bpk-button--link__NGVhY FilterButton_button__Mzc2N').click()
+      
+      // Filling the multi-search form
+      cy.get('#fsc-origin-search-0').type("b").type({downArrow}).type({Enter})
+      cy.get('#fsc-origin-search-1').type("j").type({downArrow}).type({Enter})
+      cy.get('#fsc-destination-search-0').type("h").type({downArrow}).type({Enter})
+      cy.get('#fsc-destination-search-1').type("i").type({downArrow}).type({Enter})
+      cy.get('*[class^="DateInput_DateInput--text"}').click().get('*[class^="BpkSelect_bpk-select"}').click().type({downArrow}).type({downArrow}).type({Enter})
 
       //cy.log('---Starting searching for Search field')
       //cy.get('#dropdownCompany').should('be.visible')
