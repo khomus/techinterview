00ea741f89beb1daa4f4ea3f05c13236c63deaef
khomus <bekar@tut.by>
2024-01-11 15:10:04 +0100

    Test commit
    new file:   README.cy.js

---
+++ README.cy.js
@@ -0,0 +1,11 @@
+//import {} from ""
+
+describe("Tech Interview", () => {
+    
+    it("Test 1", () =>{
+       
+        cy.visit('www.zerkalo.io')  
+        cy.get("search_from_str").should('be.visible')
+        
+    })
+})
--- README.md
+++
@@ -1,2 +0,0 @@
-# techinterview
-Tech Interviews
