12e899ad019b2e501f263a76166b6e935241da5a
khomus <bekar@tut.by>
2024-01-16 13:04:09 +0100

    CHange Tech2 to FB and log in

--- cypress/e2e/tech2.cy.js
+++ cypress/e2e/tech2.cy.js
@@ -6,7 +6,19 @@ describe("Tech Interview", () => {
   it("Test 1", () =>{
      
       cy.visit('http://www.facebook.com')  
-      cy.get('#destinationInput-input').click().get('.BpkButton_bpk-button__MDk1Y BpkButton_bpk-button--link__NGVhY FilterButton_button__Mzc2N').click()
+      cy.get('[id^=u_0_j_]').click({ force: true })
+      cy.pause(2000)
+      cy.get('#email').type('elftelleri@yahoo.com')
+      cy.pause(2000)
+      cy.get('#pass').type('elftellerivtorniki')
+      cy.pause(2000)
+      cy.get('[id^=u_0_5_]').click()
+      cy.pause()
+
+      cy.visit('http://www.facebook.com/groups/itkrakowiwroclawporusski')
+
+
+      //cy.get('#destinationInput-input').click().get('.BpkButton_bpk-button__MDk1Y BpkButton_bpk-button--link__NGVhY FilterButton_button__Mzc2N').click()
       
       // Filling the multi-search form
       //cy.get('*[class^="DateInput_DateInput--text"}').click().get('*[class^="BpkSelect_bpk-select"}').click().type({downArrow}).type({downArrow}).type({Enter})
